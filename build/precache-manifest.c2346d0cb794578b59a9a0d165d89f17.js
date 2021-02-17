0bd9ad09ffe9bdaa6c202e290b895eca3054619f
mamoussa <mamoussa@vermeg.com>
2021-02-17 21:40:44 +0100

    add build project

---
+++ build/precache-manifest.c2346d0cb794578b59a9a0d165d89f17.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6900c65e4aa187e26222aff7572d2fb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7e9ca76de4cc188b7b5",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "35c83a4ef4fe98e0b3cc",
+    "url": "/static/css/main.c781316c.chunk.css"
+  },
+  {
+    "revision": "b7e9ca76de4cc188b7b5",
+    "url": "/static/js/2.0f77fca9.chunk.js"
+  },
+  {
+    "revision": "8920b6b0ed060e86f685f8aaf55f8bc5",
+    "url": "/static/js/2.0f77fca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a68644d45102d25ad1",
+    "url": "/static/js/3.ce6f2d33.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.ce6f2d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f80b89440c4c5c1f4f",
+    "url": "/static/js/4.ed1090a8.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.ed1090a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea3a5590e909a69bdb4",
+    "url": "/static/js/5.c0cc79d0.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.c0cc79d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c83a4ef4fe98e0b3cc",
+    "url": "/static/js/main.5d1019e8.chunk.js"
+  },
+  {
+    "revision": "8476b1b21840b80b9756",
+    "url": "/static/js/runtime-main.fc329233.js"
+  }
+]);
